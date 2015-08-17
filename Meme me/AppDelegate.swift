f4b0c5bbcf8a514f4c1ba9b4fa6234f76c2e3382
Rohan <sarith21@gmail.com>
2015-08-17 07:33:22 -0700

    Removed unnecessary references to self from appDelegate

--- Meme me/AppDelegate.swift
+++ Meme me/AppDelegate.swift
@@ -29,8 +29,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
-        self.memes.append(m)
-        self.memes.append(yooda)
+        memes.append(m)
+        memes.append(yooda)
         return true
     }
 
